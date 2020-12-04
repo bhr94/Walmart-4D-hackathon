f0a03091f0eda4b6c69c13801c310ee25770e919
Bahar Mutadayin <baharmutadayin@Bahars-MacBook-Pro.local>
2020-12-04 00:45:36 -0800

    added flip functionality to the card

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,32 @@
 import React from "react";
 import Card from "./components/Card";
-import ReactCardFlip from "react-card-flip";
 import BackCard from "./components/BackCard";
-import Flippy, { FrontSide, BackSide } from "react-flippy";
+import ReactCardFlip from "react-card-flip";
 
-// https://reactjsexample.com/flipping-cards-for-your-react-projects/#google_vignette
+
 class App extends React.Component {
-  // ... component class
+  constructor() {
+    super();
+    this.state = {
+      isFlipped: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    if(!this.state.isFlipped) {
+      this.setState((prevState) => ({ isFlipped: !prevState.isFlipped }));
+    }
+   
+  }
+
   render() {
-    // .. return
     return (
-      <Flippy
-        flipOnHover={false} // default false
-        flipOnClick={true} // default false
-        flipDirection="horizontal" // horizontal or vertical
-        ref={(r) => (this.flippy = r)} // to use toggle method like this.flippy.toggle()
-        // if you pass isFlipped prop component will be controlled component.
-        // and other props, which will go to div
-        /// these are optional style, it is not necessary
-        className="card"
-      >
-        <FrontSide className = "card">
-          <Card />
-        </FrontSide>
-        <BackSide   className="card">
-          <BackCard />
-        </BackSide>
-      </Flippy>
+      <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
+        <Card flipClick={this.handleClick} />
+        <BackCard flipClick={this.handleClick} />
+      </ReactCardFlip>
     );
   }
 }
--- client/src/components/BackCard.js
+++ client/src/components/BackCard.js
@@ -2,21 +2,17 @@ import React from "react";
 import logo from "../assets/images/spark.svg";
 
 class BackCard extends React.Component {
-  state = {
-    isFlipped: false,
-  };
-
-  flipClick = () => {
-
-  };
+ 
   render() {
     return (
+        <div className="card" onClick = {this.props.flipClick}>
         <div className="card__flip">
           <div className="card__front">
             <img className="card__spark" src={logo} alt="Reveal Your Deal" />
           </div>
           <div className="card__back">
           </div>
+        </div>
         </div>
     );
   }
