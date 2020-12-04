9621df53750442e983910ccb82955304899af95c
Bahar Mutadayin <baharmutadayin@Bahars-MacBook-Pro.local>
2020-12-04 10:23:10 -0800

    added back card components

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,16 +1,16 @@
 import React from "react";
 import logo from "../assets/images/spark.svg";
+import shoe from "../assets/images/shoe.svg"
 
 class Card extends React.Component {
   render() {
     return (
-      <div className="card" onClick = {() =>this.props.flipClick(this.props.id)}>
+      <div className="card " onClick = {() =>this.props.flipClick(this.props.id)}>
         <div className="card__flip" >
           <div className="card__front">
             <img className="card__spark" src={logo} alt="Reveal Your Deal" />
           </div>
           <div className="card__back">
-            <h1 className = "flip"> this is the frontside</h1>
           </div>
         </div>
        </div>
--- client/src/components/FlipCard.js
+++ client/src/components/FlipCard.js
@@ -4,21 +4,6 @@ import BackCard from "./BackCard";
 import ReactCardFlip from "react-card-flip";
 
 class FlipCard extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       isFlipped: false,
-//     };
-//     this.handleClick = this.handleClick.bind(this);
-//   }
-
-//   handleClick(e) {
-//     e.preventDefault();
-//     if (!this.state.isFlipped) {
-//       this.setState((prevState) => ({ isFlipped: !prevState.isFlipped }));
-//     }
-//   }
-
   render() {
     return (
       <ReactCardFlip isFlipped={this.props.isFlipped} flipDirection="vertical">
