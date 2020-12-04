5476ffdff1499cddf75d3ee40e2ab44a277cf532
Bahar Mutadayin <baharmutadayin@Bahars-MacBook-Pro.local>
2020-12-03 23:02:40 -0800

    created pages folder and added reveal page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,9 @@
-import Navigation from "./components/Navigation";
-import Hero from "./components/Hero";
-import MainSection from "./components/MainSection";
+import RevealPage from "./pages/RevealPage";
 function App() {
   return (
-    <div>
-      <Navigation />
-      <Hero />
-      <MainSection/>
-    </div>
+    <>
+     <RevealPage/>
+     </>
   );
 }
 
---
+++ client/src/pages/RevealPage.js
@@ -0,0 +1,14 @@
+import Navigation from "../components/Navigation";
+import Hero from "../components/Hero";
+import MainSection from "../components/MainSection";
+function RevealPage() {
+  return (
+    <div>
+      <Navigation />
+      <Hero />
+      <MainSection />
+    </div>
+  );
+}
+
+export default RevealPage;
