7dfab49d8dd47cb31e50d43d11a796606fa2e105
Justin Cheung <justin.cheung08@gmail.com>
2020-12-04 10:24:01 -0800

    added downloadApp component

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,37 @@
-import React from "react";
-import Card from "./components/Card";
-import BackCard from "./components/BackCard";
-import ReactCardFlip from "react-card-flip";
-
+import React from "react"
+import Card from "./components/Card"
+import BackCard from "./components/BackCard"
+import ReactCardFlip from "react-card-flip"
 
 class App extends React.Component {
   constructor() {
-    super();
+    super()
     this.state = {
       isFlipped: false,
-    };
-    this.handleClick = this.handleClick.bind(this);
+    }
+    this.handleClick = this.handleClick.bind(this)
   }
 
   handleClick(e) {
-    e.preventDefault();
-    if(!this.state.isFlipped) {
-      this.setState((prevState) => ({ isFlipped: !prevState.isFlipped }));
+    e.preventDefault()
+    if (!this.state.isFlipped) {
+      this.setState((prevState) => ({ isFlipped: !prevState.isFlipped }))
     }
-   
   }
 
   render() {
     return (
-      <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
-        <Card flipClick={this.handleClick} />
-        <BackCard flipClick={this.handleClick} />
-      </ReactCardFlip>
-    );
+      <>
+        <ReactCardFlip
+          isFlipped={this.state.isFlipped}
+          flipDirection="vertical"
+        >
+          <Card flipClick={this.handleClick} />
+          <BackCard flipClick={this.handleClick} />
+        </ReactCardFlip>
+      </>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ client/src/components/DownloadApp.js
@@ -0,0 +1,32 @@
+import spark from "../assets/images/spark.svg"
+import play from "../assets/images/googleplay.svg"
+import app from "../assets/images/appstore.svg"
+
+function DownloadApp() {
+  return (
+    <section className="download-app">
+      <img className="download-app__spark" src={spark} alt="spark" />
+      <article className="download-app__main">
+        <h3 className="download-app__awesome">
+          Awesome! You've unlocked an exclusive rollback deal.
+        </h3>
+        <p className="download-app__added">
+          The deal has been added to{" "}
+          <u>
+            <strong>My Roll Backs</strong>
+          </u>
+        </p>
+        <p className="download-app__twice">
+          Want twice the deals?{" "}
+          <strong>Shop Walmart grocery on our App!</strong>
+        </p>
+        <div className="download-app__apps">
+          <img className="download-app__link" src={app} alt="App store" />
+          <img className="download-app__link" src={play} alt="Google Play" />
+        </div>
+      </article>
+    </section>
+  )
+}
+
+export default DownloadApp
