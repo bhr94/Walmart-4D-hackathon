8fcf9acbad06d138d9ded70bddd51acd499a3414
Bahar Mutadayin <baharmutadayin@Bahars-MacBook-Pro.local>
2020-12-04 00:09:46 -0800

    added flip to the card

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,33 @@
-import Card from "./components/Card"
+import React from "react";
+import Card from "./components/Card";
+import ReactCardFlip from "react-card-flip";
+import BackCard from "./components/BackCard";
+import Flippy, { FrontSide, BackSide } from "react-flippy";
 
-function App() {
-  return (
-    <div className="App">
-      <Card />
-    </div>
-  )
+class App extends React.Component {
+  // ... component class
+  render() {
+    // .. return
+    return (
+      <Flippy
+        flipOnHover={false} // default false
+        flipOnClick={true} // default false
+        flipDirection="horizontal" // horizontal or vertical
+        ref={(r) => (this.flippy = r)} // to use toggle method like this.flippy.toggle()
+        // if you pass isFlipped prop component will be controlled component.
+        // and other props, which will go to div
+        /// these are optional style, it is not necessary
+        className="card"
+      >
+        <FrontSide className = "card">
+          <Card />
+        </FrontSide>
+        <BackSide   className="card">
+          <BackCard />
+        </BackSide>
+      </Flippy>
+    );
+  }
 }
 
-export default App
+export default App;
--- client/src/components/BackCard.js
+++ client/src/components/BackCard.js
@@ -1,21 +1,25 @@
 import React from "react";
 import logo from "../assets/images/spark.svg";
 
-class Card extends React.Component {
+class BackCard extends React.Component {
+  state = {
+    isFlipped: false,
+  };
+
+  flipClick = () => {
+
+  };
   render() {
     return (
-      // <div className="card">
         <div className="card__flip">
           <div className="card__front">
             <img className="card__spark" src={logo} alt="Reveal Your Deal" />
           </div>
           <div className="card__back">
-            {/* <h1> this is the frontside</h1> */}
           </div>
         </div>
-      // </div>
     );
   }
 }
 
-export default Card;
+export default BackCard;
